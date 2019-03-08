9220b0f98315bb5c3b4232d6331ba80d6fc4d128
VinylKnight <roger.bucio@loop.colum.edu>
2019-03-07 18:21:39 -0600

    Code and Application of Raycasting

---
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Detects interactive elements the player is looking at.
+/// </summary>
+public class DetectLookedAtInteractive : MonoBehaviour
+{
+    [Tooltip("Starting point of Raycast used to detect interactive objects.")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("Determines the length from the raycast origin we will search for Interactive objects.")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, maxRange);
+    }
+}
