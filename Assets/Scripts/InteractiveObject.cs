8180586acb2c4a96a95ca8458bfdc001c61c27df
VinylKnight <roger.bucio@loop.colum.edu>
2019-03-20 21:13:09 -0500

    File Organization and The Addition of SFX Code

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
     [SerializeField]
     private string displayText = nameof(InteractiveObject);
 
     public string DisplayText => displayText;
+    private AudioSource audioSource;
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
     public void InteractWith()
     {
-        
+        try
+        {
+            audioSource.Play();
+
+        }
+        catch (System.Exception)
+        {
+
+            throw new System.Exception("Interactive Object requires an audio source.");
+        }
+       
         Debug.Log($"Player interacted with: {gameObject.name}.");
     }
 }
