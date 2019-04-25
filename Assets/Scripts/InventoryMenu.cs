68f37e8502664c803b9052585cce1192b47a163d
VinylKnight <roger.bucio@loop.colum.edu>
2019-04-25 11:45:07 -0500

    Inventory Menu Functionality Part 1

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance == null)
+                throw new System.Exception("There is currently no InventoryMenu instance, " +
+                    "but you are trying to access it! Make sure the InventoryMenu script is applied to a GameObject in your scene!");
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            throw new System.Exception("There is already an instance of InventoryMenu and there can only be one.");
+    }
+}
--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -44,5 +44,6 @@ public class InventoryObject : InteractiveObject
         PlayerInventory.InventoryObjects.Add(this);
         collider.enabled = false;
         renderer.enabled = false;
+        Debug.Log($"Inventory menu game object name {InventoryMenu.Instance.name}");
     }
 }
