a57a0dc11d9f0395366ae004bb4955f96b74eb0e
VinylKnight <roger.bucio@loop.colum.edu>
2019-05-09 14:17:20 -0500

    Addition of Flashlight and End Game Collider Trigger

---
+++ Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameTrigger : TitleMenu
+{
+    [SerializeField]
+    private Collider endGameTriggerCollider;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("UITestingScene");
+            ShowCredits();
+        }
+    }
+}
